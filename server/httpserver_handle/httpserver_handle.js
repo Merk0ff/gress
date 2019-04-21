cfe8b7dcdff65ca79d582b3fee3833194c90978e
17Варламов <17varlamov@student.dcn>
2019-04-21 23:12:40 +0000

    moved all routes to 'routes.js' file

--- server/httpserver_handle/httpserver_handle.js
+++ server/httpserver_handle/httpserver_handle.js
@@ -21,8 +21,7 @@
  */
 
 
-/** @const @private A file system handle module. */
-const Fs = require('fs');
+
 
 /** @const @private Express module. */
 const Exp = require('express');
@@ -30,61 +29,13 @@ const Exp = require('express');
 /** @const @private Express app. */
 const App = new Exp();
 
-/** @const @private Endless scroll unit. */
-const EndlessScroll = require('./endlessScroll');
-
-
-/**
- * Send file to client function
- * @param {Object} res A response param.
- * @param {Object} path Path to file.
- */
-function sendFile(res, path) {
-  Fs.readFile(path,
-      function(err, data) {
-        if (err) {
-          res.writeHead(500);
-          return res.end('Error loading ' + path);
-        }
-        res.writeHead(200);
-        res.end(data);
-      });
-}
-
-/**
- * Server handle function.
- */
-function serverHandler() {
-  App.get('/', function(req, res) {
-    sendFile(res, '../client/project.html');
-    console.log('sombody once told me');
-  });
-
-
-  /**
-     * this function is used to serve ststic files
-     * go to https://expressjs.com/en/starter/static-files.html to read more
-     */
-  App.use(Exp.static('../client'));
-
-
-  /**
-     * this request is needed for infinite scroll
-     * server sends json file
-     */
-  App.get('/package.json', function(req, res) {
-    EndlessScroll.sendJSON(res);
-    console.log('json sent with offset' /* +offset */);
-  });
-}
 
 /**
  * Set up the Server
  * @param {int} port Server port.
  */
 exports.setUp = function(port = 3000) {
-  serverHandler();
-
+  require('./routes')(App);
   App.listen(port, function() {
     console.log('listening on *:' + port);
   });
