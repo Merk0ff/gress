ca19039a6122f5f0944089907a3dd429adcf0e1f
Ksenia Rogova <ksenia.dumpling@gmail.com>
2019-05-07 23:07:58 +0000

    Update sessions.js

--- client/js/sessions.js
+++ client/js/sessions.js
@@ -1,17 +1,38 @@
+/**
+ * FIRST VERSION. IT WILL BE REWRITTEN
+ *
+ * Function that send email and password
+ * to the server by post request
+ *
+ * @param {Object} jQuery. -- ???
+ */
 $(document).ready(function(){
     var email,pass;
     $("#submit").click(function(){
+        console.log('Enter submit');
         email=$("#myEmail").val();
         pass=$("#myPassword").val();
-        /*
-        * Perform some validation here.
-        */
+
+        /*$.ajax({
+            type: 'POST',
+            url: '/login',
+            data: {email:email, pass:pass},
+            success: function(data) {
+                window.alert('Im here!'); // need to rewrite
+                if(data==='done') {
+                    //just example
+                    //window.location.href="/admin";
+                    window.alert('Done');
+                }
+            },
+        }); */
+
         $.post("/login",{email:email,pass:pass},function(data){
-            windows.alert('Im here!'); // need to rewrite
+            window.alert('Im here!'); // line for debug
             if(data==='done') {
                 //just example
                 //window.location.href="/admin";
-                windows.alert('Done');
+                window.alert('Done');
             }
         });
     });
