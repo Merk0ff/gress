991ffd84622db9242ee261c5b639668524bc14a4
Ksenia Rogova <ksenia.dumpling@gmail.com>
2019-05-14 21:17:42 +0000

    Upload New File

---
+++ client/js/registration.js
@@ -0,0 +1,46 @@
+/**
+ * FIRST VERSION. IT WILL BE REWRITTEN
+ *
+ * Function that send info about new profile
+ * to the server by post request
+ *
+ * @param {Object} jQuery. -- ???
+ */
+
+$(document).ready(function(){
+    let investorBool;
+    let firstName, lastName;
+    let email, pass;
+    let phoneNum;
+    let education;
+    let links;
+
+    $("#submit").click(function(){
+        investorBool = $("#InvestorBlock").val(); // Dunno is it gonna work
+        firstName = $("#FirstNameBlock").val();
+        lastName = $("#LastNameBlock").val();
+        email = $("#EmailBlock").val();
+        pass = $("#PasswordBlock").val();
+        phoneNum = $("#Phone").val();
+        education = $("#InlineFormCustomSelectPref").val(); // Dunno is it gonna work
+        links = $("#Portfolio").val();
+
+        $.ajax({
+            type: 'POST',
+            url: '/signup',
+            data: {investor: investorBool, name: firstName,
+                   surname:lastName, email:email, pass:pass,
+                   phone: phoneNum, education: education,
+                   links: links },
+            success: function(data) {
+                window.alert('Im here!'); // need to rewrite
+                if(data==='done') {
+                    //just example
+                    //window.location.href="/admin";
+                    window.alert('Done');
+                }
+            },
+        });
+
+    });
+});
